341d38289339dac60ac8bc011b27cfbb5fe620e5
hsmukesh <159506763+hsmukesh@users.noreply.github.com>
2024-04-12 12:33:51 +0530

    Update Exp4_JKflipflop.v

--- Exp4_JKflipflop.v
+++ Exp4_JKflipflop.v
@@ -1,1 +1,18 @@
-
+module jkflipflop(clk,reset,j,k,q);
+input clk,reset,j,k;
+output reg q;
+always@(posedge clk)
+begin
+if(reset)
+q<=1'b0;
+else begin
+case({j,k})
+2'b00:q<=q;
+2'b01:q<=1'b0;
+2'b10:q<=1'b1;
+2'b11:q<=~q;
+default:q=1'bx;
+endcase
+end
+end
+endmodule
