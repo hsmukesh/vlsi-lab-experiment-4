6ae3207a06c0c8896537a0d43692ae94e62a2d6e
hsmukesh <159506763+hsmukesh@users.noreply.github.com>
2024-04-21 21:04:21 +0530

    Update Exp4_Mod10counter.v

--- Exp4_Mod10counter.v
+++ Exp4_Mod10counter.v
@@ -1,1 +1,12 @@
-
+module mod10counter(clk,rst,count);
+input clk,rst;
+output[3:0]count;
+reg[3:0]count;
+always@(posedge clk)
+begin
+if(rst|count==4'b1001)
+count<=4'b0;
+else
+count<=count+1;
+end
+endmodule
